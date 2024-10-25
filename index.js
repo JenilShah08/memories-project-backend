eb74971fb8c4d7a5ab4d7d49a09de8a7788ea876
Jenil Shah <85637539+JenilShah08@users.noreply.github.com>
2024-10-25 19:27:10 +0530

    Added login/signup functionality, added tokenization, and functionality that only user who have account in memories can like the memories and the creator of the memories can delete it.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
-import dotenv from 'dotenv';
+import userRouter from "./routes/user.js";
 
 const app = express();
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(cors());
 
 // Make sure this line stays below the app.use(cors());
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello to memories API');
@@ -21,12 +23,14 @@ app.get('/', (req, res) => {
 // https://www.mongodb.com/cloud/atlas
 // MongoDB connection
 // const CONNECTION_URL = "mongodb+srv://kaleyashash02:kaleyashash02123@cluster0.bhqc9xh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const PORT = process.env.PORT || 5000;
 
-// const PORT = 5000;
+const PORT = process.env.PORT || 5000; // Use this when deploying on vercel
 
-console.log(process.env.CONNECTION_URL);
-console.log(PORT);
+// const PORT = 5000; // Use this when using localhost
+
+// console.log(process.env.CONNECTION_URL);
+// console.log('PORT:', process.env.PORT);
+// console.log(PORT);
 
 mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
